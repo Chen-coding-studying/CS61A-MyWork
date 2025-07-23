eaf25e9852f03e31a6e826afd84de7c65ea2992d
Chen-coding-studying <Zhukai.Chen24@student.xjtlu.edu.cn>
2025-07-23 19:33:15 +0800

    19:33

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -91,4 +91,17 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    print(n)
+    t = 0 
     while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        print(n)
+        t += 1
+    return t + 1
+        
+              
+         
+         

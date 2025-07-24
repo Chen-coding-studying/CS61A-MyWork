b91b13bcb9813bc45deaeebe4a8f588b9312c055
Chen-coding-studying <Zhukai.Chen24@student.xjtlu.edu.cn>
2025-07-24 15:35:03 +0800

    7.24

---
+++ In class/01.py
@@ -0,0 +1,41 @@
+def identity(k):
+    return k 
+
+def cube(k):
+    return pow(k, 3)
+
+def summation(n,term):
+    total, k = 0, 1
+    while k <= n :
+        total, k = total + term(k), k + 1
+    return total 
+
+
+
+
+
+
+
+
+def sum_naturals(n):
+    """Sum
+   
+    total,k = 0,1
+    while k <= n:
+        total, k = total + k, k + 1
+    return total
+    """
+    return summation(n, identity)
+
+
+
+
+def sum_cubes(n):
+
+    """
+    total, k = 0,1
+    while k <= n:
+        total, k = total + pow(k,3), k + 1
+    return total        
+    """
+    return summation(n,cube)
